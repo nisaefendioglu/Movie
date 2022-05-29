161ac833f9591a38d15133ce5d514e1d626ea199
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-05-29 12:43:29 +0300

    🎥

--- app/src/main/java/com/nisaefendioglu/movieapp/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/movieapp/MainActivity.kt
@@ -2,10 +2,66 @@ package com.nisaefendioglu.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nisaefendioglu.movieapp.adapter.MovieAdapter
+import com.nisaefendioglu.movieapp.databinding.ActivityMainBinding
+import com.nisaefendioglu.movieapp.viewmodel.MovieViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MovieViewModel by viewModels()
+    private lateinit var movieAdapter: MovieAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        setUpRv()
+
+    }
+
+
+    private fun setUpRv() {
+        movieAdapter = MovieAdapter()
+        binding.recyclerView.apply {
+            adapter = movieAdapter
+            layoutManager = LinearLayoutManager(
+                this@MainActivity, LinearLayoutManager.HORIZONTAL,
+                false
+            )
+
+            setHasFixedSize(true)
+        }
+
+        binding.rvEpisodes.apply {
+            adapter = movieAdapter
+            layoutManager = LinearLayoutManager(
+                this@MainActivity, LinearLayoutManager.HORIZONTAL,
+                false
+            )
+
+            setHasFixedSize(true)
+        }
+
+
+        binding.rvRecentlyAdded.apply {
+            adapter = movieAdapter
+            layoutManager = LinearLayoutManager(
+                this@MainActivity, LinearLayoutManager.HORIZONTAL,
+                false
+            )
+
+            setHasFixedSize(true)
+        }
+
+
+        viewModel.responseMovie.observe(this) { listMovie ->
+            movieAdapter.movie = listMovie
+        }
+
     }
 }
